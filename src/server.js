416ef8abd906264d37ae32b2e4321f58b08cddf5
Daariimaa <daarii@Daariimaas-MacBook-Pro.local>
2022-01-26 01:29:48 +0800

    Fixed pages, added search component

--- src/server.js
+++ src/server.js
@@ -1,38 +1,36 @@
-const express = require( 'express' );
-const next    = require( 'next' );
+const express = require("express");
+const next = require("next");
 
 // Import middleware.
-const routes = require( './routes' );
+const routes = require("./routes");
 
 // Setup app.
-const app     = next({ dev: 'production' !== process.env.NODE_ENV } );
-const handle  = app.getRequestHandler();
-const handler = routes.getRequestHandler( app );
+const app = next({ dev: "production" !== process.env.NODE_ENV });
+const handle = app.getRequestHandler();
+const handler = routes.getRequestHandler(app);
 
-app.prepare()
-  .then( () => {
+app.prepare().then(() => {
+  // Create server.
+  const server = express();
 
-    // Create server.
-    const server = express();
+  // Use our handler for requests.
+  server.use(handler);
 
-    // Use our handler for requests.
-    server.use(handler);
+  // Don't remove. Important for the server to work. Default route.
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
 
-    // Don't remove. Important for the server to work. Default route.
-    server.get( '*', ( req, res ) => {
-      return handle( req, res );
-    } );
+  // Get current port.
+  const port = process.env.PORT || 8080;
 
-    // Get current port.
-    const port = process.env.PORT || 8080;
+  // Error check.
+  server.listen(port, (err) => {
+    if (err) {
+      throw err;
+    }
 
-    // Error check.
-    server.listen( port, err => {
-      if ( err ) {
-        throw err;
-      }
-
-      // Where we starting, yo!
-      console.log( `> Ready on port ${port}...` );
-    } );
-  } );
+    // Where we starting, yo!
+    console.log(`> Ready on port ${port}...`);
+  });
+});
