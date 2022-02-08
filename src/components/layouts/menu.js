20c9e7661b61c146a4fed1d314d19fe5560d4d16
Gift_zwerg3 <bulgantamir@nmma.co>
2022-02-08 12:59:49 +0800

    update

--- src/components/layouts/menu.js
+++ src/components/layouts/menu.js
@@ -1,11 +1,42 @@
 import React from "react";
-import axios from "axios";
-import Config from "../../config";
+import Link from "next/link";
 
 class MenuComponent extends React.Component {
   render() {
     const { mainMenu, topMenu } = this.props;
-    return <div className="main-header">Here will be menu</div>;
+    return (
+      <div className="main-header">
+        <Link href="/">
+          <a>
+            <img className="logo" src="/images/mms-logo.png" alt="logo" />
+          </a>
+        </Link>
+        <div className="menus">
+          <div className="topMenu">
+            <div className="topMenuList">
+              {topMenu.items.map((item, ind) => {
+                return (
+                  <Link key={ind} href={`/${item.slug}`}>
+                    <a>{item.title}</a>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <div className="mainMenu">
+            <div className="mainMenuList">
+              {mainMenu.items.map((item, ind) => {
+                return (
+                  <Link key={ind} href={`/${item.slug}`}>
+                    <a>{item.title}</a>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
